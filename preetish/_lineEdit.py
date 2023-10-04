7dce10042c45e97c6fbaf0734d256dfa3164224e
e19cse313@bennett.edu.in <80623083+preetish-simhadri@users.noreply.github.com>
2023-10-04 23:12:19 +0530

    created few more lineedits

--- preetish/_lineEdit.py
+++ preetish/_lineEdit.py
@@ -54,7 +54,64 @@ class MyWidget(QWidget):
         self.lineEdit3.setPlaceholderText("Write Here")
         self.lineEdit3.setMinimumSize(300,10)
         self.lineEdit3.setStyleSheet("""
-        
+                                        QLineEdit {
+                                            width: 210px;
+                                            height: 50px;
+                                            background: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:0 rgba(227, 213, 255), stop:1 rgba(255, 231, 231));
+                                            border-radius: 30px;
+                                            letter-spacing: 0.8px;
+                                            padding-left: 15px;
+                                            cursor: pointer;
+                                            box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.075);
+                                        }
+
+                                        QLineEdit::hover {
+                                            background: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:0 rgba(197, 184, 255), stop:1 rgba(255, 211, 211));
+                                        }
+
+                                        QLineEdit:focus {
+                                            border: 2px solid rgb(255, 81, 0);
+                                        }
+        """)
+
+        self.lineEdit4 = QLineEdit()
+        self.lineEdit4.setMinimumSize(300,10)
+        self.lineEdit4.setPlaceholderText("Write Here")
+        self.lineEdit4.setStyleSheet("""
+                                        QLineEdit {
+                                            color: #8707ff;
+                                            border: 2px solid #8707ff;
+                                            border-radius: 10px;
+                                            padding: 10px 25px;
+                                            background: transparent;
+                                            max-width: 190px;
+                                            }
+
+                                        QLineEdit:active {
+                                            shadow: 2px 2px 15px #8707ff inset;
+                                            }
+
+        """)
+
+        self.lineEdit5 = QLineEdit()
+        self.lineEdit5.setMinimumSize(300,10)
+        self.lineEdit5.setPlaceholderText("Write Here")
+        self.lineEdit5.setStyleSheet("""
+                                        QLineEdit {
+                                            border: 2px solid transparent;
+                                            width: 15em;
+                                            height: 2.5em;
+                                            padding-left: 0.8em;
+                                            outline: none;
+                                            background-color: #F3F3F3;
+                                            border-radius: 10px;
+                                        }
+
+                                        QLineEdit:hover, QLineEdit:focus {
+                                            border: 2px solid #4A9DEC;
+                                            background-color: white;
+                                            selection-background-color: #4A9DEC;
+                                        }
         """)
 
 
@@ -62,8 +119,8 @@ class MyWidget(QWidget):
         layout.addWidget(self.lineEdit1, alignment=Qt.AlignCenter)
         layout.addWidget(self.lineEdit2, alignment=Qt.AlignCenter)
         layout.addWidget(self.lineEdit3, alignment=Qt.AlignCenter)
-        # layout.addWidget(self.lineEdit4, alignment=Qt.AlignCenter)
-        # layout.addWidget(self.lineEdit5, alignment=Qt.AlignCenter)
+        layout.addWidget(self.lineEdit4, alignment=Qt.AlignCenter)
+        layout.addWidget(self.lineEdit5, alignment=Qt.AlignCenter)
 
 def main():
     app = QApplication(sys.argv)
