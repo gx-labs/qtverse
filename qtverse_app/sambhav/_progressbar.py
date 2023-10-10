e1b75a1a4bd7fd048aeef122662d4366e3140f71
sambhavpathak13 <sambhavpathaksp@gmail.com>
2023-10-10 11:55:51 +0530

    Completed Progressbar widget

--- qtverse_app/sambhav/_progressbar.py
+++ qtverse_app/sambhav/_progressbar.py
@@ -34,14 +34,52 @@ class PracWidget(QWidget):
                                         text-align: center;
                                     }
                                     QProgressBar:chunk{
-                                        background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0.1 #32CD32, stop:0.2 transparent, stop:0.3 #32CD32, stop:0.4 transparent, stop:0.5 #32CD32, stop:0.6 transparent, stop:0.8 transparent, stop:0.9 #32CD32, stop:0.95 #32CD32);
-                                        border-radius: 5px;    
+                                        background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0.1 #32CD32, stop:0.2 yellow, stop:0.3 #32CD32, stop:0.4 yellow, stop:0.5 #32CD32, stop:0.6 yellow, stop:0.8 yellow, stop:0.9 #32CD32, stop:0.98 #32CD32);
+                                        border-radius: 5px;
+                                        margin: 0.5px;   
+                                    }
+        ''')
+
+        self.prgsbar3 = QProgressBar()
+        self.prgsbar3.setValue(56)
+        self.prgsbar3.setStyleSheet('''
+                                    QProgressBar{
+                                        border: 2px solid brown;
+                                        border-radius: 5px;
+                                        text-align: center;
+                                        font-weight: bold;
+                                        color: red;
+                                        font-size: 18px; 
+                                    }
+                                    QProgressBar:chunk{
+                                        background-color: pink;
+                                        width: 5px;
+                                        margin: 0.5px;
+                                    }
+        ''')
+
+        self.prgsbar4 = QProgressBar()
+        self.prgsbar4.setValue(56)
+        self.prgsbar4.setStyleSheet('''
+                                    QProgressBar{
+                                        border: 2px solid brown;
+                                        border-radius: 5px;
+                                        text-align: center;
+                                        font-weight: bold;
+                                        color: yellow; 
+                                    }
+                                    QProgressBar:chunk{
+                                        background-color: #00ecff;
+                                        border-radius: 5px;
+                                        margin: 0.5px;
                                     }
         ''')
 
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.prgsbar1)
         self.layout.addWidget(self.prgsbar2)
+        self.layout.addWidget(self.prgsbar3)
+        self.layout.addWidget(self.prgsbar4)
         self.setLayout(self.layout)
 
 def main():
