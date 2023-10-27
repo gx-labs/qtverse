0407508cbd73a25a92f449383a11e8fc3d1e5f89
esha01 <eshaanand.ea1701@gmail.com>
2023-10-27 12:49:25 +0530

    added functionality to combobx

--- !rnd/esha/pyviewer/05pyviewer_widgetsLoading.py
+++ !rnd/esha/pyviewer/05pyviewer_widgetsLoading.py
@@ -8,16 +8,16 @@ class pyviewer(QWidget):
         super().__init__()
 
         # main layout
-        self.setWindowTitle('pyviewer')
-        self.setFixedSize(1200,800)
+        self.setWindowTitle('dviewer')
+        self.setFixedSize(1200, 800)
         main_layout = QHBoxLayout(self)
         self.setLayout(main_layout)
 
         # tree widget
         self.tree_widget = QTreeWidget(self)
-        self.tree_widget.setFixedSize(250,800)
+        self.tree_widget.setFixedSize(250, 800)
         main_layout.addWidget(self.tree_widget)
-        self.tree_widget.setHeaderLabels(['pyviewer'])
+        self.tree_widget.setHeaderLabels(['dviewer'])
         self.tree_widget.itemDoubleClicked.connect(self.on_itemDoubleClicked)
 
         # content widget to display UI instances
@@ -27,35 +27,33 @@ class pyviewer(QWidget):
         self.content_layout.setContentsMargins(0, 0, 0, 0)
 
         self.root_path = 'qtverse\widgets\src\WIDGETS'
-        self.item_actions = {}   # storing actions associated with tree items
-
         self.tree_items(self.root_path, self.tree_widget)
 
-    def tree_items(self, path, parent):   # recursively populating the tree widget with items
-        for entry in os.listdir(path):   # loops over entries in the current directory
-            entry_path = os.path.join(path, entry)
+    def tree_items(self, path, parent): # recursively populating the tree widget with items
+        for entry in os.listdir(path): # loops over entries in the current directory
+            widget_filepath = os.path.join(path, entry)
 
-            if os.path.isdir(entry_path) and not entry.endswith("__pycache__"): # creates parent item and calls tree_items recursively for the subdirectory
+            if os.path.isdir(widget_filepath) and not entry.endswith("__pycache__"): # creates parent item and calls tree_items recursively for the subdirectory
                 parentItem = QTreeWidgetItem(parent)
                 parentItem.setText(0, entry)
-                self.tree_items(entry_path, parentItem)
+                self.tree_items(widget_filepath, parentItem)
 
-            elif entry.endswith('.py'): # creates a child item
+            elif entry.endswith('.py'): #create child item
                 child = QTreeWidgetItem(parent)
                 child.setText(0, entry)
-                self.item_actions[entry] = self.action_function(entry, path) # associates action with the child item
+                action = self.action_function(widget_filepath)
+                child.setData(0, 1, action)  # Storing action function as item data
 
-    def action_function(self, filename, parent_path):    
+    def action_function(self, widget_filepath):    
         def action():
-            class_name = os.path.splitext(filename)[0] # extracting the class name 
-            module_path = os.path.join(parent_path, f'{class_name.lower()}.py') # constructing the module path 
+            file_name = os.path.splitext(os.path.basename(widget_filepath))[0]  # extracting the class name 
 
             try:
-                spec = importlib.util.spec_from_file_location(class_name, module_path)
+                spec = importlib.util.spec_from_file_location(file_name, widget_filepath)
                 module = importlib.util.module_from_spec(spec)  # creates an empty module
                 spec.loader.exec_module(module) # executes the module 
 
-                ui_class = getattr(module, class_name)
+                ui_class = getattr(module, file_name)
                 ui_instance = ui_class()
 
                 # clears the content layout and add the new ui instance
@@ -73,8 +71,7 @@ class pyviewer(QWidget):
         return action
 
     def on_itemDoubleClicked(self, item, column):
-        filename = item.text(0)
-        action = self.item_actions.get(filename)
+        action = item.data(0, 1)
         if action:
             action()
 
--- apps/dviewer/01main.py
+++ apps/dviewer/01main.py
@@ -1,6 +1,7 @@
+from PySide2.QtWidgets import QApplication, QTreeWidget, QTreeWidgetItem, QWidget, QVBoxLayout, QComboBox, QPushButton, QHBoxLayout, QScrollArea
+from PySide2.QtCore import Qt
 import os
 import sys
-from PySide2.QtWidgets import QApplication, QTreeWidget, QTreeWidgetItem, QHBoxLayout, QWidget
 import importlib.util
 
 class pyviewer(QWidget):
@@ -13,11 +14,45 @@ class pyviewer(QWidget):
         main_layout = QHBoxLayout(self)
         self.setLayout(main_layout)
 
+        # viewer widget
+        self.viewer = QWidget(self)
+        self.viewer.setFixedSize(320, 800)
+        main_layout.addWidget(self.viewer)
+        self.viewer_layout = QVBoxLayout(self.viewer)
+
+        # combobox
+        self.combobox = QComboBox()
+        self.combobox.addItem("ESH")
+        self.combobox.addItem("PRT")
+        self.combobox.addItem("SAM")
+        self.combobox.addItem("SHB")
+        self.combobox.currentIndexChanged.connect(self.update_tree_visibility)
+        self.viewer_layout.addWidget(self.combobox, alignment=Qt.AlignLeft)
+
+        # status buttons
+        self.statusBtn_layout = QHBoxLayout()
+        self.viewer_layout.addLayout(self.statusBtn_layout)
+
+        self.view_all = QPushButton("ALL")
+        self.statusBtn_layout.addWidget(self.view_all)
+        self.wip = QPushButton("wip")
+        self.statusBtn_layout.addWidget(self.wip)
+        self.submitted = QPushButton("submitted")
+        self.statusBtn_layout.addWidget(self.submitted)
+        self.review = QPushButton("review")
+        self.statusBtn_layout.addWidget(self.review)
+        self.approved = QPushButton("approved")
+        self.statusBtn_layout.addWidget(self.approved)
+
         # tree widget
-        self.tree_widget = QTreeWidget(self)
-        self.tree_widget.setFixedSize(250, 800)
-        main_layout.addWidget(self.tree_widget)
-        self.tree_widget.setHeaderLabels(['dviewer'])
+        self.tree_scroll_area = QScrollArea(self)
+        self.tree_scroll_area.setWidgetResizable(True)
+        self.viewer_layout.addWidget(self.tree_scroll_area)
+
+        self.tree_widget = QTreeWidget()
+        self.tree_scroll_area.setWidget(self.tree_widget)
+        self.tree_widget.setFixedSize(300, 800)
+        self.tree_widget.setHeaderLabels([''])
         self.tree_widget.itemDoubleClicked.connect(self.on_itemDoubleClicked)
 
         # content widget to display UI instances
@@ -26,37 +61,47 @@ class pyviewer(QWidget):
         self.content_layout = QHBoxLayout(self.content_widget)
         self.content_layout.setContentsMargins(0, 0, 0, 0)
 
-        self.root_path = 'C:\\PROJECTS\\PySide\\qtverse\\qtverse\\widgets\\src\\WIDGETS'
+        # self.root_path = 'C:\\PROJECTS\\PySide\\qtverse\\qtverse\\widgets\\src\\WIDGETS'
+        self.root_path = 'qtverse\widgets\src\WIDGETS'
         self.tree_items(self.root_path, self.tree_widget)
 
-    def tree_items(self, path, parent): # recursively populating the tree widget with items
-        for entry in os.listdir(path): # loops over entries in the current directory
+        # Set all items of the tree widget to be expanded
+        self.expand_all_items(self.tree_widget.invisibleRootItem())
+
+    def tree_items(self, path, parent):  
+        for entry in os.listdir(path):
             widget_filepath = os.path.join(path, entry)
 
-            if os.path.isdir(widget_filepath) and not entry.endswith("__pycache__"): # creates parent item and calls tree_items recursively for the subdirectory
+            if os.path.isdir(widget_filepath) and not entry.endswith("__pycache__"):
                 parentItem = QTreeWidgetItem(parent)
                 parentItem.setText(0, entry)
                 self.tree_items(widget_filepath, parentItem)
+                parentItem.setExpanded(True)
 
-            elif entry.endswith('.py'): #create child item
+            elif entry.endswith('.py'):
                 child = QTreeWidgetItem(parent)
                 child.setText(0, entry)
                 action = self.action_function(widget_filepath)
-                child.setData(0, 1, action)  # Storing action function as item data
+                child.setData(0, 1, action)
 
-    def action_function(self, widget_filepath):    
+    def update_tree_visibility(self, index):
+        selected_item_text = self.combobox.itemText(index)
+        for i in range(self.tree_widget.topLevelItemCount()):
+            item = self.tree_widget.topLevelItem(i)
+            item.setHidden(item.text(0) != selected_item_text)
+
+    def action_function(self, widget_filepath):
         def action():
-            file_name = os.path.splitext(os.path.basename(widget_filepath))[0]  # extracting the class name 
+            file_name = os.path.splitext(os.path.basename(widget_filepath))[0]
 
             try:
                 spec = importlib.util.spec_from_file_location(file_name, widget_filepath)
-                module = importlib.util.module_from_spec(spec)  # creates an empty module
-                spec.loader.exec_module(module) # executes the module 
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
 
                 ui_class = getattr(module, file_name)
                 ui_instance = ui_class()
 
-                # clears the content layout and add the new ui instance
                 while self.content_layout.count():
                     item = self.content_layout.takeAt(0)
                     widget = item.widget()
@@ -74,6 +119,12 @@ class pyviewer(QWidget):
         action = item.data(0, 1)
         if action:
             action()
+
+    def expand_all_items(self, item):
+        item.setExpanded(True)
+        for i in range(item.childCount()):
+            child_item = item.child(i)
+            self.expand_all_items(child_item)
 
 def main():
     app = QApplication([])
