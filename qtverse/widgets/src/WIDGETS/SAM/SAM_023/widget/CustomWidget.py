2740146093bf9129ba6377a5023bedac97ceb4a7
sambhavpathak13 <sambhavpathaksp@gmail.com>
2023-11-03 16:57:47 +0530

    added more progressbar widgets.

---
+++ qtverse/widgets/src/WIDGETS/SAM/SAM_023/widget/CustomWidget.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+from PySide2.QtWidgets import *
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+
+css_file = os.path.join(os.path.dirname(__file__), "style.css")
+if os.path.exists(css_file):
+    with open(css_file, 'r') as f:
+        css_data = f.read()
+else:
+    print("CSS File does not exist")
+
+class CustomWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        layout = QVBoxLayout()
+
+        self.prgsbar = QProgressBar()
+        self.prgsbar.setValue(50)
+        self.prgsbar.setFixedSize(400, 25)
+        self.prgsbar.setStyleSheet(css_data)
+
+        self.setLayout(layout)
+        layout.addWidget(self.prgsbar, alignment=Qt.AlignCenter)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    widget = CustomWidget()
+    widget.show()
+    sys.exit(app.exec_())
