4d4e9f4a042ac431a703932b74df242fd3b0e531
Jash <y.j.naiduofficial@gmail.com>
2023-12-19 17:15:08 +0530

    final sidebar without functioning code

--- qtweb/qtweb_0.0/src/components/SideBar.jsx
+++ qtweb/qtweb_0.0/src/components/SideBar.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
 import styled from "styled-components";
 
 const SidebarContainer = styled.div`
-  width: 650px; 
+  width: 650px;
   // needs to be corrected when made final changes
   background-color: #ccc;
   padding: 0px 2px;
@@ -48,94 +47,68 @@ const SidebarItem = styled.li`
 `;
 
 const Sidebar = () => {
-  const [expandedFolders, setExpandedFolders] = useState([]);
-
-  const toggleFolder = (folder) => {
-    setExpandedFolders((prevFolders) => {
-      if (prevFolders.includes(folder)) {
-        // If folder is already in the list, remove it
-        return prevFolders.filter((f) => f !== folder);
-      } else {
-        // If folder is not in the list, add it
-        return [...prevFolders, folder];
-      }
-    });
-  };
-
   return (
     <SidebarContainer>
       <SidebarHeader>WIDGETS</SidebarHeader>
 
-      <SidebarMainFolder onClick={() => toggleFolder("Widgets")}>
-        Widgets
-      </SidebarMainFolder>
-      {expandedFolders.includes("Widgets") && (
-        <ul>
-          <SidebarItem>Buttons</SidebarItem>
-          <SidebarItem>Radio Buttons</SidebarItem>
-          <SidebarItem>Check Box </SidebarItem>
-          <SidebarItem>Combo Box </SidebarItem>
-          <SidebarItem>Group Box </SidebarItem>
-          <SidebarItem>Spin Box </SidebarItem>
-          <SidebarItem>Tool Box</SidebarItem>
-          <SidebarItem>Sliders </SidebarItem>
-          <SidebarItem>Line Edit </SidebarItem>
-          <SidebarItem>Calendar </SidebarItem>
-          <SidebarItem>List View </SidebarItem>
-          <SidebarItem>Table View</SidebarItem>
-          <SidebarItem>Tree View </SidebarItem>
-          <SidebarItem>Loading Bars</SidebarItem>
-          <SidebarItem>Tab Widget</SidebarItem>
-          <SidebarItem>List Widget</SidebarItem>
-          <SidebarItem>Labels</SidebarItem>
-          <SidebarItem>Widgets 2</SidebarItem>
-          <SidebarItem>QDockWidget</SidebarItem>
-          <SidebarItem>QSizeGrip</SidebarItem>
-          <SidebarItem>QHeaderView</SidebarItem>
-          <SidebarItem>QSplitter</SidebarItem>
-          <SidebarItem>QToolButton</SidebarItem>
-          <SidebarItem>QToolTip</SidebarItem>
-          <SidebarItem>QMainWindow</SidebarItem>
-          <SidebarItem>QMenu</SidebarItem>
-          <SidebarItem>QWidget</SidebarItem>
-        </ul>
-      )}
+      <SidebarMainFolder>Widgets</SidebarMainFolder>
 
-      <SidebarMainFolder onClick={() => toggleFolder("Dialog")}>
-        Dialogs
-      </SidebarMainFolder>
-      {expandedFolders.includes("Dialog") && (
-        <ul>
-          <SidebarItem>Input Dialog </SidebarItem>
-          <SidebarItem>Color Dialog</SidebarItem>
-          <SidebarItem>File Dialog </SidebarItem>
-          <SidebarItem>Font Dialog </SidebarItem>
-        </ul>
-      )}
+      <ul>
+        <SidebarItem>Buttons</SidebarItem>
+        <SidebarItem>Radio Buttons</SidebarItem>
+        <SidebarItem>Check Box </SidebarItem>
+        <SidebarItem>Combo Box </SidebarItem>
+        <SidebarItem>Group Box </SidebarItem>
+        <SidebarItem>Spin Box </SidebarItem>
+        <SidebarItem>Tool Box</SidebarItem>
+        <SidebarItem>Sliders </SidebarItem>
+        <SidebarItem>Line Edit </SidebarItem>
+        <SidebarItem>Calendar </SidebarItem>
+        <SidebarItem>List View </SidebarItem>
+        <SidebarItem>Table View</SidebarItem>
+        <SidebarItem>Tree View </SidebarItem>
+        <SidebarItem>Loading Bars</SidebarItem>
+        <SidebarItem>Tab Widget</SidebarItem>
+        <SidebarItem>List Widget</SidebarItem>
+        <SidebarItem>Labels</SidebarItem>
+        <SidebarItem>Widgets 2</SidebarItem>
+        <SidebarItem>QDockWidget</SidebarItem>
+        <SidebarItem>QSizeGrip</SidebarItem>
+        <SidebarItem>QHeaderView</SidebarItem>
+        <SidebarItem>QSplitter</SidebarItem>
+        <SidebarItem>QToolButton</SidebarItem>
+        <SidebarItem>QToolTip</SidebarItem>
+        <SidebarItem>QMainWindow</SidebarItem>
+        <SidebarItem>QMenu</SidebarItem>
+        <SidebarItem>QWidget</SidebarItem>
+      </ul>
 
-      <SidebarMainFolder onClick={() => toggleFolder("Bars")}>
-        Bars
-      </SidebarMainFolder>
-      {expandedFolders.includes("Bars") && (
-        <ul>
-          <SidebarItem>Scroll Bars </SidebarItem>
-          <SidebarItem>Menu Bar</SidebarItem>
-          <SidebarItem>Tool Bar </SidebarItem>
-          <SidebarItem>Status Bar</SidebarItem>
-          <SidebarItem>Tab Bar </SidebarItem>
-        </ul>
-      )}
+      <SidebarMainFolder>Dialogs</SidebarMainFolder>
+
+      <ul>
+        <SidebarItem>Input Dialog </SidebarItem>
+        <SidebarItem>Color Dialog</SidebarItem>
+        <SidebarItem>File Dialog </SidebarItem>
+        <SidebarItem>Font Dialog </SidebarItem>
+      </ul>
+
+      <SidebarMainFolder>Bars</SidebarMainFolder>
+
+      <ul>
+        <SidebarItem>Scroll Bars </SidebarItem>
+        <SidebarItem>Menu Bar</SidebarItem>
+        <SidebarItem>Tool Bar </SidebarItem>
+        <SidebarItem>Status Bar</SidebarItem>
+        <SidebarItem>Tab Bar </SidebarItem>
+      </ul>
 
       <SidebarHeader>CUSTOM</SidebarHeader>
 
-      <SidebarMainFolder onClick={() => toggleFolder("Custom")}>
-        Custom
-      </SidebarMainFolder>
-      {expandedFolders.includes("Custom") && (
-        <ul>
-          <SidebarItem>Custom </SidebarItem>
-        </ul>
-      )}
+      <SidebarMainFolder>Custom</SidebarMainFolder>
+
+      <ul>
+        <SidebarItem>Custom </SidebarItem>
+      </ul>
     </SidebarContainer>
   );
 };
