7dad0dc9bf6f5a6854b90e84ba01b7645f5a8ddb
Jash <y.j.naiduofficial@gmail.com>
2023-12-27 11:25:55 +0530

    app.js with browser router addition

--- qtweb/practise/qt_demo_0/src/App.jsx
+++ qtweb/practise/qt_demo_0/src/App.jsx
@@ -2,15 +2,24 @@ import React from 'react'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 
 const App = () => {
   return (
     <>
-      <NavBar />
-      <Home/>
-      <Footer />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/subscribe" element={<ProSubscription />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} /> */}
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
