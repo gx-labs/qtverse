0238946ee25f0dd8a71ed7999a2ab39374b1a021
esha01 <eshaanand.ea1701@gmail.com>
2024-01-03 13:49:55 +0530

    #7 added more round buttons with icons

---
+++ qtverse/widgets/src/WIDGETS/ESH/ESH_258/widget/CustomWidget.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+from PySide2.QtWidgets import *
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+
+css_file = os.path.join(os.path.dirname(__file__), "style.css")
+if os.path.exists(css_file):
+    with open(css_file, 'r') as f:
+        css_data = f.read()
+else:
+    print("CSS File does not exist")
+
+class CustomWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        layout = QVBoxLayout()
+
+        self.button = QPushButton("➜")  
+        self.button.setFixedSize(50, 50)
+        self.button.setStyleSheet(css_data)
+
+        layout.addWidget(self.button)  
+        self.setLayout(layout)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    widget = CustomWidget()
+    widget.show()
+    sys.exit(app.exec_())
+
+
+
+
