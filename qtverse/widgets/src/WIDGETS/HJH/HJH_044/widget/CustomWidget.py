3c3c9e78c28a3999ae8df96c5875fbd831737e61
Hriday96 <hridayjhaveri@gmail.com>
2024-01-12 16:20:44 +0530

    Added radio buttons

---
+++ qtverse/widgets/src/WIDGETS/HJH/HJH_044/widget/CustomWidget.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+from PySide2.QtWidgets import *
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+
+css_file = os.path.join(os.path.dirname(__file__), "style.css")
+if os.path.exists(css_file):
+    with open(css_file, 'r') as f:
+        css_data = f.read()
+else:
+    print("CSS File does not exist")
+
+class CustomWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        frame = QFrame(self)
+        frame.setFixedSize(QSize(250, 60))
+
+        layout = QHBoxLayout(frame)
+
+        self.button1 = QRadioButton('Student')
+        self.button1.setChecked(True)
+        self.button1.setFixedSize(95, 35)
+        self.button2 = QRadioButton('Teacher')
+        self.button2.setFixedSize(95, 35)
+
+        layout.addWidget(self.button1)
+        layout.addWidget(self.button2)
+
+        frame.setLayout(layout)
+
+        central_layout = QVBoxLayout(self)
+        central_layout.addWidget(frame)
+        
+        self.setStyleSheet(css_data)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    widget = CustomWidget()
+    widget.show()
+    sys.exit(app.exec_())
