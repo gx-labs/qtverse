d706a896f559523d917c1f85080e5f471cd92fb3
esha01 <eshaanand.ea1701@gmail.com>
2024-01-23 18:06:11 +0530

    #7 added new progress bars

---
+++ qtverse/widgets/src/WIDGETS/ESH/ESH_604/widget/CustomWidget.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+from PySide2.QtWidgets import *
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+
+css_file = os.path.join(os.path.dirname(__file__), "style.css")
+if os.path.exists(css_file):
+    with open(css_file, 'r') as f:
+        css_data = f.read()
+else:
+    print("CSS File does not exist")
+
+class CustomWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        layout = QVBoxLayout()
+
+        self.progress_bar = QProgressBar()
+        self.progress_bar.setValue(20)
+        self.progress_bar.setFixedSize(400, 14)
+        self.progress_bar.setStyleSheet(css_data)
+
+        layout.addWidget(self.progress_bar)
+        self.setLayout(layout)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    widget = CustomWidget()
+    widget.show()
+    sys.exit(app.exec_())
+
+
+
+
