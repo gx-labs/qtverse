3b26c313ebe2c9f9a4cc8a62471c62bea7e30de4
Hriday96 <hridayjhaveri@gmail.com>
2024-01-30 10:19:30 +0530

    Added radio buttons

---
+++ qtverse/widgets/src/WIDGETS/HJH/HJH_073/widget/CustomWidget.py
@@ -0,0 +1,52 @@
+import os
+import sys
+
+from PySide2.QtWidgets import *
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+
+css_file = os.path.join(os.path.dirname(__file__), "style.css")
+if os.path.exists(css_file):
+    with open(css_file, 'r') as f:
+        css_data = f.read()
+else:
+    print("CSS File does not exist")
+
+class CustomWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        layout = QHBoxLayout(self)
+
+        self.radiobutton1 = QRadioButton('Next')
+        self.radiobutton1.setChecked(True)
+        self.radiobutton1.setFixedSize(70, 30)
+        self.radiobutton1.clicked.connect(self.radiobutton1_pressed)
+        self.radiobutton2 = QRadioButton('Svelte')
+        self.radiobutton2.setFixedSize(70, 30)
+        self.radiobutton2.clicked.connect(self.radiobutton2_pressed)
+        self.radiobutton3 = QRadioButton('Remix')
+        self.radiobutton3.setFixedSize(70, 30)
+        self.radiobutton3.clicked.connect(self.radiobutton3_pressed)
+
+        layout.addWidget(self.radiobutton1)
+        layout.addWidget(self.radiobutton2)
+        layout.addWidget(self.radiobutton3)
+
+        self.setLayout(layout)
+        self.setStyleSheet(css_data)
+        
+    def radiobutton1_pressed(self):
+        print ('Pressed radiobutton1!')
+        
+    def radiobutton2_pressed(self):
+        print ('Pressed radiobutton2!')
+        
+    def radiobutton3_pressed(self):
+        print ('Pressed radiobutton3!')
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    widget = CustomWidget()
+    widget.show()
+    sys.exit(app.exec_())
