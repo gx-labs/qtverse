a4ce197951b4b354403983fb36c6c274f02ed25b
Hriday96 <hridayjhaveri@gmail.com>
2024-02-02 14:19:21 +0530

    #42: Added function to resolve icon path

--- qtviewer/ui/utils.py
+++ qtviewer/ui/utils.py
@@ -9,3 +9,9 @@ def read_yaml(filepath):
             info_data = yaml.safe_load(file)
         return info_data
     return None
+
+def qt_icon(filename):
+    icon_directory = os.getenv("QT_ICON_PATH")
+    resolved_icon_directory = os.path.abspath(os.path.join(icon_directory, filename))
+    print(resolved_icon_directory)
+    return resolved_icon_directory
