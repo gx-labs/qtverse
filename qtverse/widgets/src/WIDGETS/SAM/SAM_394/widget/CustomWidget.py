a15e4dec8a1c3ded8dc5061425958370a634856b
sambhavpathak13 <sambhavpathaksp@gmail.com>
2024-02-27 18:07:51 +0530

    added new message box styles

---
+++ qtverse/widgets/src/WIDGETS/SAM/SAM_394/widget/CustomWidget.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+from PySide2.QtWidgets import *
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+
+css_file = os.path.join(os.path.dirname(__file__), "style.css")
+if os.path.exists(css_file):
+    with open(css_file, 'r') as f:
+        css_data = f.read()
+else:
+    print("CSS File does not exist")
+
+
+class CustomWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        layout = QVBoxLayout()
+
+        self.messagebox = QMessageBox()
+        self.messagebox.setWindowTitle("Styled MessageBox")
+        self.message_text = "<div style='text-align: center;'><span style='font-size: 30px; color: #A5DC86;'>✓</span><span style='font-size: 18px; color: black; font-weight:bold;'> Confirm Title</span><br><span style='font-size: 16px; color: grey;'>Confirm Message</span></div>"
+        self.messagebox.setText(self.message_text)
+        self.messagebox.setStyleSheet(css_data)
+
+        self.shadow = QGraphicsDropShadowEffect()
+        self.shadow.setColor("rgb(115, 115, 115)")
+        self.shadow.setBlurRadius(7)
+        self.shadow.setOffset(1, 2)
+        self.messagebox.setGraphicsEffect(self.shadow)
+
+        # Set the standard buttons before accessing them
+        self.messagebox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+
+        # Get the QPushButton objects
+        ok_button = self.messagebox.button(QMessageBox.Ok)
+        cancel_button = self.messagebox.button(QMessageBox.Cancel)
+
+        # Set the text for the QPushButton objects
+        ok_button.setText("Okay")
+        cancel_button.setText("Cancel")
+
+        self.messagebox.accepted.connect(self.ok_button_clicked)
+        self.messagebox.rejected.connect(self.cancel_button_clicked)
+
+        self.setLayout(layout)
+        layout.addWidget(self.messagebox, alignment=Qt.AlignCenter)
+
+    def ok_button_clicked(self):
+        print("Ok button clicked")
+
+    def cancel_button_clicked(self):
+        print("Cancel button clicked")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    widget = CustomWidget()
+    widget.show()
+    sys.exit(app.exec_())
